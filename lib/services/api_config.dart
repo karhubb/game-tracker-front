03af3cce15313c2a7a7788d17a32f90230d27f86
karhubb <karlasof71@gmail.com>
2026-05-05 08:47:23 -0600

    Fix render url

--- lib/services/api_config.dart
+++ lib/services/api_config.dart
@@ -3,7 +3,7 @@ class ApiConfig {
   // y dejamos Render comentado como fallback de producción para volver a activarlo luego.
   // static const String _defaultUrl = 'http://localhost:8080';
   // URL de Render para producción:
-  static const String _defaultUrl = 'https://game-tracker-backend-a6ec.onrender.com';
+  static const String _defaultUrl = 'https://game-tracker-back.onrender.com';
 
   static String get baseUrl {
     const fromEnv = String.fromEnvironment('API_BASE_URL');
